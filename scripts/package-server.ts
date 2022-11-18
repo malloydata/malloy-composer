33cac9ef989225d00a031e7a56f9bb16092e5526
Michael Aden <maden@google.com>
2022-11-18 23:25:58 +0000

    Add package versioning and all-targets flag.

--- scripts/package-server.ts
+++ scripts/package-server.ts
@@ -34,7 +34,8 @@ async function packageServer(
   platform: string,
   architecture: string,
   sign = true,
-  skipPackageStep = false
+  skipPackageStep = false,
+  version = "dev"
 ) {
   let target = `${platform}-${architecture}`;
   doBuild(target);
@@ -68,8 +69,25 @@ async function packageServer(
     "--target",
     `${nodeTarget}-${target}`,
     "--output",
-    `pkg/malloy-composer-cli-${target}`,
+    `pkg/malloy-composer-cli-${target}-${version}`,
   ]);
+}
+
+/**
+ * @returns Array of version bits. [major, minor, patch]
+ */
+function getVersionBits(): Array<number> {
+  return JSON.parse(fs.readFileSync("package.json", "utf-8"))
+    .version.split(".")
+    .map(Number);
+}
+
+type Architecture = "x64" | "arm64";
+type Platform = "darwin" | "linux" | "win32";
+
+interface PackageTarget {
+  architecture: Architecture;
+  platform: Platform;
 }
 
 (async () => {
@@ -78,40 +96,65 @@ async function packageServer(
     .option("-p, --platform <string>", "Target platform")
     .option("-a, --arch <string>", "Target architecture")
     .option("--skip-package", "Skip packaging step")
-    .option("--sign", "Sign the build executable");
+    .option("--sign", "Sign the build executable")
+    .option(
+      "--all-targets",
+      "create all supported platform/binary combinations"
+    );
 
   program.parse();
   const options = program.opts();
   let platform: string;
   let architecture: string;
 
-  if (options.platform) {
-    platform = options.platform;
+  let targets: PackageTarget[];
+  if (options.allTargets) {
+    targets = [
+      { platform: "darwin", architecture: "x64" },
+      { platform: "darwin", architecture: "arm64" },
+      { platform: "linux", architecture: "x64" },
+    ];
   } else {
-    platform = process.platform;
-    console.log(
-      `Target platform was not specified, using current: ${platform}`
-    );
-  }
+    if (options.platform) {
+      platform = options.platform;
+    } else {
+      platform = process.platform;
+      console.log(
+        `Target platform was not specified, using current: ${platform}`
+      );
+    }
 
-  if (options.arch) {
-    architecture = options.arch;
-  } else {
-    architecture = process.arch;
-    console.log(
-      `Target architecture was not specified, using current: ${architecture}`
-    );
+    if (options.arch) {
+      architecture = options.arch;
+    } else {
+      architecture = process.arch;
+      console.log(
+        `Target architecture was not specified, using current: ${architecture}`
+      );
+    }
+    targets = [
+      {
+        platform: platform as Platform,
+        architecture: architecture as Architecture,
+      },
+    ];
   }
 
   console.log(JSON.stringify(options));
-
-  console.log(`Packaging server for ${platform}-${architecture}`);
-  await packageServer(
-    platform,
-    architecture,
-    options.sign,
-    options.skipPackage
-  );
+  const versionBits = getVersionBits();
+  versionBits[2] = Math.floor(Date.now() / 1000);
+  for (const target of targets) {
+    console.log(
+      `Packaging server for ${target.platform}-${target.architecture}`
+    );
+    await packageServer(
+      target.platform,
+      target.architecture,
+      options.sign,
+      options.skipPackage,
+      versionBits.join(".")
+    );
+  }
 })()
   .then(() => {
     console.log("Composer server built successfully");
