ec6c43100c116313b51b5d7475547f63e80fea3b
Will Scullin <scullin@meta.com>
2024-09-06 17:22:17 -0700

    lint
    Signed-off-by: Will Scullin <scullin@meta.com>

--- scripts/malloy-packages.js
+++ scripts/malloy-packages.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
-const package = require('../package.json');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const package = require("../package.json");
 
-let malloyPackages = Object.keys(package.dependencies).filter(package =>
-  package.startsWith('@malloy')
+let malloyPackages = Object.keys(package.dependencies).filter((package) =>
+  package.startsWith("@malloy")
 );
 
 if (process.argv.length === 3) {
   malloyPackages = malloyPackages.map(
-    package => `${package}@${process.argv[2]}`
+    (package) => `${package}@${process.argv[2]}`
   );
 }
-console.log(malloyPackages.join(' '));
+// eslint-disable-next-line no-console
+console.log(malloyPackages.join(" "));
