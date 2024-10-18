57b21fc06591051bd595b07d7f430aafb19809ce
Will Scullin <scullin@meta.com>
2024-10-17 18:02:17 -0700

    Actually test a couple things

--- packages/query-composer/tests/core/query.spec.ts
+++ packages/query-composer/tests/core/query.spec.ts
@@ -7,8 +7,34 @@
 
 import {QueryBuilder} from '../../src/core/query';
 
+import {source} from '../../example/example_model';
+
 describe('QueryBuilder', () => {
   it('constructs', () => {
-    expect(new QueryBuilder(undefined)).not.toBeNull();
+    const qb = new QueryBuilder(source);
+    expect(qb).not.toBeNull();
+    expect(qb.isEmpty()).toBe(true);
+  });
+
+  describe('addField', () => {
+    it('Adds a dimension', () => {
+      const qb = new QueryBuilder(source);
+      expect(qb.isEmpty()).toBe(true);
+      qb.addField({stageIndex: 0}, 'name');
+      expect(qb.isEmpty()).toBe(false);
+      expect(qb.getQueryStringForNotebook()).toEqual(
+        'run: names -> {\n  group_by: name\n}'
+      );
+    });
+
+    it('Adds a measure', () => {
+      const qb = new QueryBuilder(source);
+      expect(qb.isEmpty()).toBe(true);
+      qb.addField({stageIndex: 0}, 'population');
+      expect(qb.isEmpty()).toBe(false);
+      expect(qb.getQueryStringForNotebook()).toEqual(
+        'run: names -> {\n  aggregate: population\n}'
+      );
+    });
   });
 });
